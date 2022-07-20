f829261d68acf62e8c43c6f2a78b312eebec33e2
Ksenija Blanusa <xenija.zivkovic@gmail.com>
2022-07-20 15:54:07 -0700

    VSL-66: Implemented depositNFT function for treasury (#42)
    * VSL-66: Removed borrow functions, added depositNFT function to treasury
    
    * VSL-66: Added depositTokens
    
    * VSL-66: Updated according to PR comments
    
    * VSL-66: Added missing import
    
    * VSL-66: Removed depositVault because of duplicate code, removed duplicate transaction and updated the send flow to treasury transaction
    
    * VSL-66: Reverted depositVault function, created flow vault with create treasury transactions, updated FE transaction

--- packages/client/src/flow/sendFlowToTreasury.tx.js
+++ packages/client/src/flow/sendFlowToTreasury.tx.js
@@ -3,17 +3,19 @@ export const SEND_FLOW_TO_TREASURY = `
 	import FungibleToken from 0xFungibleToken
 
 	transaction(treasuryAddr: Address, amount: UFix64) {
-		
+  
 		prepare(signer: AuthAccount) {
+			
 			let vault = signer.borrow<&FungibleToken.Vault>(from: /storage/flowTokenVault)!
 			let tokens <- vault.withdraw(amount: amount)
-			let treasuryAcct = getAccount(treasuryAddr)
-			let pubCapability = treasuryAcct.getCapability(DAOTreasury.TreasuryPublicPath)
-											.borrow<&DAOTreasury.Treasury{DAOTreasury.TreasuryPublic}>()
-											?? panic("A DAOTreasury doesn't exist here.")
-
-			
-			pubCapability.depositVault(vault: <- tokens)
+		
+			let treasury = getAccount(treasuryAddr).getCapability(DAOTreasury.TreasuryPublicPath)
+						.borrow<&DAOTreasury.Treasury{DAOTreasury.TreasuryPublic}>()
+						?? panic("A DAOTreasury doesn't exist here.")
+		
+			let identifier: String = vault.getType().identifier
+		
+			treasury.depositTokens(identifier: identifier, vault: <- tokens)
 		}
 	}
 `;
--- transactions/create_treasury.cdc
+++ transactions/create_treasury.cdc
@@ -1,12 +1,18 @@
 import DAOTreasury from "../contracts/DAOTreasury.cdc"
+import FlowToken from "../contracts/core/FlowToken.cdc"
 
 transaction(initialSigners: [Address], initialThreshold: UInt64) {
   
   prepare(signer: AuthAccount) {
-    signer.save(<- DAOTreasury.createTreasury(initialSigners: initialSigners, initialThreshold: initialThreshold), to: DAOTreasury.TreasuryStoragePath)
+    let treasury <- DAOTreasury.createTreasury(initialSigners: initialSigners, initialThreshold: initialThreshold)
+
+    // Seed Treasury with commonly used vaults
+    let flowVault <- FlowToken.createEmptyVault()
+
+    treasury.depositVault(vault: <- flowVault)
+
+    // Save Treasury to the account
+    signer.save(<- treasury, to: DAOTreasury.TreasuryStoragePath)
     signer.link<&DAOTreasury.Treasury{DAOTreasury.TreasuryPublic}>(DAOTreasury.TreasuryPublicPath, target: DAOTreasury.TreasuryStoragePath)
-  }
-  execute {
-   
   }
 }
