441dbc2f998d3d0488ddc67bda419979e35e70dc
Linxiao Li <lilinxiaover1990@gmail.com>
2022-08-23 12:42:02 -0700

    add addvault and add collection components back (#81)

--- packages/client/src/components/SafeSettings/index.js
+++ packages/client/src/components/SafeSettings/index.js
@@ -4,18 +4,29 @@ import { Web3Context } from "contexts/Web3";
 import SafeDetailsSetting from "./components/SafeDetailsSetting";
 import SignatureThreshold from "./components/SignatureThreshold";
 import Owners from "./components/Owners";
+import Assets from "./components/Assets";
 
 const SafeSettings = () => {
   const location = useLocation();
   const treasuryAddress = location.pathname.split("/")[2];
   const web3 = useContext(Web3Context);
   const treasury = web3?.treasuries?.[treasuryAddress];
-  const { setTreasury, proposeRemoveSigner } = web3;
+  const {
+    setTreasury,
+    proposeRemoveSigner,
+    proposeAddVault,
+    proposeAddCollection,
+  } = web3;
   return (
     <React.Fragment>
       <SafeDetailsSetting treasury={treasury} setTreasury={setTreasury} />
       <SignatureThreshold treasury={treasury} />
       <Owners treasury={treasury} proposeRemoveSigner={proposeRemoveSigner} />
+      <Assets
+        treasury={treasury}
+        proposeAddVault={proposeAddVault}
+        proposeAddCollection={proposeAddCollection}
+      />
     </React.Fragment>
   );
 };
