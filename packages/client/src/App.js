111e59801a55f4cdaae4c10d44c91b155e8c629a
Manny404 <manny@art404.com>
2022-08-24 14:17:45 -0400

    switch to HashRouter for IPFS deploys (#83)

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.sass";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { GraphQLClient, ClientContext } from "graphql-hooks";
 import { Web3Provider, ModalProvider } from "./contexts";
 import { Home, Safe, LoadSafe, CreateSafe } from "./pages";
