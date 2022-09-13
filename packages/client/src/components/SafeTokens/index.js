d488378f1e9eb1f3b43ce275169245c76412b0b0
linxiao-li <lilinxiaover1990@gmail.com>
2022-09-13 12:29:09 -0700

    table view done

--- packages/client/src/components/SafeTokens/index.js
+++ packages/client/src/components/SafeTokens/index.js
@@ -1,16 +1,27 @@
 import { useContext } from "react";
 import { useLocation } from "react-router-dom";
 import { Web3Context } from "contexts/Web3";
+import VaultTable from "./VaultTable";
 const SafeTokens = () => {
   const location = useLocation();
   const treasuryAddress = location.pathname.split("/")[2];
   const web3 = useContext(Web3Context);
-  const vaults = web3?.balances?.[treasuryAddress];
+  const balances = web3?.balances?.[treasuryAddress] ?? {};
+  const vaults = Object.entries(balances).map(([coinType, balance]) => ({
+    coinType,
+    balance,
+  }));
   return (
     <div>
-      <div className="is-flex mt-5">
-        <h2>Tokens</h2>
+      <div className="is-flex is-justify-content-space-between mt-5">
+        <h2 className="flex-1">Tokens</h2>
+        <div>
+          <button className="button is-secondary is-small">
+            Manage Token Vaults
+          </button>
+        </div>
       </div>
+      {vaults.length > 0 && <VaultTable vaults={vaults} />}
     </div>
   );
 };
