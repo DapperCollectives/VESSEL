1bdfd3160d83c055c1db65f15605eaf643bf6ba6
Ksenija Blanusa <xenija.zivkovic@gmail.com>
2022-09-23 14:30:11 -0700

    Reverted change  (#116)
    * Reverted change after merge, added collection init on treasury init
    
    * Reverted updating the initialize treasury transaction

--- packages/client/src/constants/constants.js
+++ packages/client/src/constants/constants.js
@@ -1,5 +1,5 @@
-export const REGULAR_LIMIT = 60;
-export const CREATE_TREASURY_LIMIT = 77;
+export const REGULAR_LIMIT = 80;
+export const CREATE_TREASURY_LIMIT = 150;
 export const SIGNER_APPROVE_LIMIT = 250;
 export const SIGNED_LIMIT = 300;
 export const EXECUTE_ACTION_LIMIT = 350;
