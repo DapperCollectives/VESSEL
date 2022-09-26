b380606436ecc9d52d476d69a04e733f702da6fb
linxiao-li <lilinxiaover1990@gmail.com>
2022-09-26 16:21:36 -0700

    configure eslint and babel to work with dynamic imports

--- packages/client/src/components/SendTokens/index.js
+++ packages/client/src/components/SendTokens/index.js
@@ -1,11 +1,9 @@
 import { SendTokensContextProvider } from "./sendTokensContext";
 import Steps from "./steps";
 
-const SendTokens = (props) => {
-  return (
-    <SendTokensContextProvider {...props}>
-      <Steps />
-    </SendTokensContextProvider>
-  );
-};
+const SendTokens = (props) => (
+  <SendTokensContextProvider {...props}>
+    <Steps />
+  </SendTokensContextProvider>
+);
 export default SendTokens;
